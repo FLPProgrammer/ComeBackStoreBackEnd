fe0dbb858cf7e239858567cf4407c327b462c077
Felipe <lplpfwr@gmail.com>
2026-03-08 12:50:16 -0300

    removing .js files

--- src/routes/passwordRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { PasswordController } from '../controllers/passwordController';
-import { PasswordService } from '../services/passwordService';
-export const passwordRoutes = Router();
-const passwordService = new PasswordService();
-const passwordController = new PasswordController(passwordService);
-passwordRoutes.post('/forgot-password', passwordController.forgot.bind(passwordController));
-passwordRoutes.post('/reset-password', passwordController.reset.bind(passwordController));
--- src/utils/mail.js
+++
@@ -1,8 +0,0 @@
-import nodemailer from 'nodemailer';
-export const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    }
-});
